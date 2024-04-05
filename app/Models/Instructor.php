bcfd839dde3e7d385aecc1d830ab7120b7b12939
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-05 17:58:25 +0200

    create instructor table

---
+++ app/Models/Instructor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instructor extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'instructor_name',
+        'job',
+        'experience',
+    ];
+    public function category(){
+
+        return $this->belongsTo(Category::class);
+    }
+
+}
