d139cc32b908879a24dfa79e848aa8cadf2b6368
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-05 04:05:17 +0200

    add cources for category( still can't access to course_id xxxxx)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -23,26 +23,36 @@ class AdminController extends Controller
         dd($request->all());
     
 
-        $diplomaName=$request->name;
-        $diplomaPrice=$request->price;
-        $diplomaDuration=$request->duration;
-        $diplomaPhoto=$request->file('photo')->store('','public');
-    
-        Category::create(
-            [
-                'category_name'=>$diplomaName,
-                'price'=>$diplomaPrice,
-                'duration'=>$diplomaDuration,
-                'image'=>$diplomaPhoto
-            ]
-        );
-        // $lastId =Category::all()->last()->id +1;
-        // CategoryCourse::create(
+        // $diplomaName=$request->name;
+        // $diplomaPrice=$request->price;
+        // $diplomaDuration=$request->duration;
+        // $diplomaPhoto=$request->file('photo')->store('','public');
+          // Category::create(
         //     [
-        //         'category_id'=>$lastId,
-        //         'course_id'=>
+        //         'category_name'=>$diplomaName,
+        //         'price'=>$diplomaPrice,
+        //         'duration'=>$diplomaDuration,
+        //         'image'=>$diplomaPhoto
         //     ]
-        //  );
+        // );
+        $diploma = new Category();
+        $diploma->category_name =$request->name;
+        $diploma->price =$request->price;
+        $diploma->duration =$request->duration;
+        $diploma->image = $request->file('photo')->store('','public');
+
+        $diploma->save();
+        
+        $lastInsertedId = $diploma->id;
+        // dd($lastInsertedId);
+      
+        CategoryCourse::create(
+            [
+                'category_id'=>$lastInsertedId,
+                'course_id'=>2,
+
+            ]
+         );
 
         // return to_route("products.index")->with("requestStatus","product created successfuly");
 
@@ -53,8 +63,12 @@ class AdminController extends Controller
         // dd($categoryID);
         $category = Category::find($categoryID);
         // dd($category);
+        $category->courses()->detach();
+
         $category->delete();
         return to_route("diplomas.create");
+
+        
     }
 
 }
