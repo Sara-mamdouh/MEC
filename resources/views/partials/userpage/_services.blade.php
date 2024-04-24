89c2ccb2a24e572a46a9268406bc22814fcc3b68
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-24 23:40:59 +0200

    create structure of services section

---
+++ resources/views/partials/userpage/_services.blade.php
@@ -0,0 +1,60 @@
+<section class="services" id="services">
+  <div class="container services__layout ">
+    <div class="services__img">
+      <img
+      src="{{asset('image/skills/lab.gif')}}"
+        alt=" image gif"
+      />
+    </div>
+    <div class="services__dec">
+      <div>
+        <h2>Grow your skills learn with us anywhere</h2>
+        <p class="services__dec__content">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+          cupiditate facere sint architecto Ad nobis soluta neque nostrum
+          suscipit beatae consequatur modi minima quasi. Assumenda,
+          inventore quis! Quibusdam, nulla cupiditate.
+        </p>
+      </div>
+      
+      <div class="services__dec__points">
+        <div>
+          <div class="services__dec__points__icon">
+          </div>
+          <div class="services__dec__points__p">
+            <p>Expert trainers
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="services__dec__points__icon services__dec__points__icon--orange">
+          </div>
+          <div class="services__dec__points__p">
+            <p>Online learning
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="services__dec__points__icon services__dec__points__icon--pink">
+          </div>
+          <div class="services__dec__points__p">
+            <p>Lifetime access
+            </p>
+          </div>
+        </div>
+        <div>
+          <div class="services__dec__points__icon services__dec__points__icon--purple">
+          </div>
+          <div class="services__dec__points__p">
+            <p>Great results
+            </p>
+          </div>
+        </div>
+
+
+       
+      </div>
+    </div>
+    
+  </div>
+</section>
