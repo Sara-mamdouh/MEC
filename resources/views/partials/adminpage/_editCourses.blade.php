eb6e98325dc4873e357f6dbf01a513cbadd45401
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-25 00:00:27 +0200

    create structure of courses form (adminpage) (edit)

---
+++ resources/views/partials/adminpage/_editCourses.blade.php
@@ -0,0 +1,27 @@
+<section class="admin-container">
+  <div class="category-form ">
+    <h1><span>edit</span> course</h1>
+    <form action="{{ route("courses.update",$course->id) }}" method="post" >
+      @csrf
+      @method("put")
+      <div class="input-field">
+        <label for="courses_name" class="form-label">name</label>
+        <input type="text" class="input-control" id="courses_name" name="name" value="{{ $course->name_course}}">
+      </div>
+    
+      <div class="input-field">
+        <label for="courses_topics" class="form-label">topics</label>
+        <input type="text" class="input-control" id="courses_topics" name="topics" value="{{ $course->topics}}">
+      </div>
+    
+
+      <div class="btn-submit">       
+        <input type="submit" value="update" name="submit">
+      </div>
+    </form>
+  </div>
+</section>
+
+
+
+
