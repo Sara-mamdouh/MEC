d940bf5e6f8b17b6ced2036681eb3ca274769d1c
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-24 23:51:26 +0200

    create structure of  gallery section(userpage)

---
+++ resources/views/partials/userpage/_gallery.blade.php
@@ -0,0 +1,42 @@
+<section class="gallery" id="gallery">
+<div class="container">
+  <div class="gallery__head">
+    <h2> <span>our</span> gallery</h2>
+  </div>
+  <div class="gallery-slider swiper">
+    <div class="swiper-wrapper">
+
+      <div class=" swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery1.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery2.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery3.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery4.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery5.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery7.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery8.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery9.jpg')}}" alt="cc">
+      </div>
+      <div class="swiper-slide gallery-slider__slide">
+        <img src="{{asset('image/gallery/gallery11.jpg')}}" alt="cc">
+      </div>
+    </div>
+    
+
+  </div>
+
+</div>
+</section>
