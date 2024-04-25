61f8b811d165f6652482fc5bb4ce6d9fb93a0de1
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-25 05:20:13 +0200

    create course factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\CourseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,6 +24,8 @@ class DatabaseSeeder extends Seeder
         
     $this->call([
         UserSeeder::class,
+        CourseSeeder::class,
+
     ]);
     }
 }
