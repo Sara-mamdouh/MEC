d66e73120281e5d83bc74eb2a4951725e9d69b5d
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-26 20:38:54 +0300

    add section hello (admin page)

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,14 +8,19 @@
   <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
- 
-
 
   @vite(['resources/css/app.css','public/scss/app.scss','resources/js/app.js'])
   <title>user page</title>
 </head>
 <body>
 @include("partials.adminpage._sidebar")
+
+<div class="admin-container hello">
+  <div class="hello-admin">
+    <img src="{{asset("image/hello.gif")}} " alt="">
+  </div>
+
+</div>
 
 
 
