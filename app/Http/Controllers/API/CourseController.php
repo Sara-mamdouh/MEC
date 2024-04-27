2c5318d144818508bf83bfff1ecdff460fbb0a1e
sara mamdoh <sara.mamdoh2020@gmail.com>
2024-04-27 03:25:41 +0300

    implement api for courses

---
+++ app/Http/Controllers/API/CourseController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $allCourses = Course::all();
+
+        return response()->json(["courses"=>$allCourses]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $newCategory = Course::create([
+            "name_course"=>$request->input('name_course'),
+            "topics"=>$request->input('topics'),
+
+        ]);
+        $res =[
+            "message" => "Course created successfully",
+            "data"=>$newCategory
+        ];
+        return response()->json($res , 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        $course = Course::find($request->route("course"));
+        if(!$course) return response()->json(["message" => "course not found"] , 404);
+
+        $res =[
+            "message" => "course below",
+            "data"=>$course
+        ];
+        return response()->json($res , 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Course $course)
+    {
+        $course->update([
+            "name_course"=>$request->name_course,
+            "topics"=>$request->topics,
+          
+        ]);
+        $res =[
+            "message" => "course updated",
+            "data"=>$course
+        ];
+        return response()->json($res , 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course)
+    {
+        //
+        $course->delete();
+        $res =[
+            "message" => "course deleted successfully",
+        ];
+        return response()->json($res);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\CourseController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResources([
-    "categories"=>CategoryController::class
+    "categories"=>CategoryController::class,
+    "courses"=>CourseController::class,
+
 ]);
 
